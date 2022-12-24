36c6d86de55f25c82ffce8ecbeaacd7d6abcd9d7
Jonah <jghebremichael95@gmail.com>
2022-12-23 21:11:24 -0500

    Finished

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Jonah";
+            int age = 27;
+            decimal height = 6.4m;
+            char initial = 'M';
+            bool b = true;
+            double d = 9.09;
+
+
+            Console.WriteLine($"Hello, {name} you are {age} years old your middle initial is {initial}. It is {b} that you are {height}ft. The meal at Bojangels was {d}$ today");
         }
     }
 }
